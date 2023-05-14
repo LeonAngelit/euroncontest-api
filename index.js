6ff2eddeb75764adb64287df3d3603fea1680969
LeonAngelit <leo63582@gmail.com>
2023-05-14 03:10:14 +0200

    remove unnecessary imports

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const routerApi = require('./routes/index');
 const cors = require('cors');
-const CountryService = require('./services/countries.service');
 require('dotenv').config();
-const service = new CountryService();
 const app = express();
 const port = process.env.PORT || 3020;
 const {
